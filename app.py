5899102100db3dca14b31acd4c664da1345b2cf4
Fernando Aracena <aracenafernandou@gmail.comgit config --global user.name Fernando>
2023-05-12 14:21:24 +0200

    adding connection string and container

--- app.py
+++ app.py
@@ -5,39 +5,29 @@ import googletrans
 import requests
 import os
 from PyPDF2 import PdfReader
-import os
-
 
 API_KEY = os.getenv('API_KEY')
-
-# Use the api_key in your code
-
+STORAGE_CONNECTION_STRING = os.getenv('STORAGE_CONNECTION_STRING')
+STORAGE_CONTAINER_NAME = os.getenv('STORAGE_CONTAINER_NAME')
 
 app = Flask(__name__)
 
- 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         # Get the uploaded file from the user
         file = request.files['file']
-        # Save the file to a temporary location
-        file_path = os.path.join('uploads', file.filename)
-        file.save(file_path)
 
         # Extract the text from the uploaded file
         text = ''
-        if file_path.endswith('.docx'):
-            text = docx2txt.process(file_path)
-        elif file_path.endswith('.pdf'):
-            with open(file_path, 'rb') as f:
-                pdf_reader = PdfReader(f)
-                for page_num in range(len(pdf_reader.pages)):
-                    page = pdf_reader.pages[page_num]
-                    text += page.extract_text()
-        elif file_path.endswith('.txt'):
-            with open(file_path, 'r') as f:
-                text = f.read()
+        if file.filename.endswith('.docx'):
+            text = docx2txt.process(file)
+        elif file.filename.endswith('.pdf'):
+            pdf_reader = PdfReader(file)
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+        elif file.filename.endswith('.txt'):
+            text = file.read().decode('utf-8')
 
         if not text:
             return "Ingen tekst ble funnet i filen."
@@ -77,10 +67,16 @@ def index():
                         print(f"Summary after translation: {translated_text}")
                         return "Kunne ikke oversette sammendraget."
                     else:
+                        # Store the file in Azure Blob Storage
+                        from azure.storage.blob import BlobServiceClient
+
+                        blob_service_client = BlobServiceClient.from_connection_string(STORAGE_CONNECTION_STRING)
+                        container_client = blob_service_client.get_container_client(STORAGE_CONTAINER_NAME)
+                        blob_client = container_client.upload_blob(name=file.filename, data=file)
+
                         return render_template('index.html', summary=translated_text)
 
     return render_template('upload.html')
-
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
