c80c45be9529ebe9797061049e4524806cf1d923
Fernando Aracena <aracenafernandou@gmail.comgit config --global user.name Fernando>
2023-05-21 13:42:24 +0200

    adding functionality for long text

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import requests
 import os
 from PyPDF2 import PdfReader
 import uuid
+from googletrans import Translator
 
 
 API_KEY = os.getenv('API_KEY')
@@ -49,30 +50,27 @@ def index():
             # Authenticate with the OpenAI API and generate a summary of the text
             openai.api_key = API_KEY
 
-            prompt = f"Please summarize the following article:\n{text}\n\nSummary:"
+            # Split the input text into smaller chunks and process each chunk separately
             try:
-                completions = openai.Completion.create(
-                    engine="text-davinci-002",
-                    prompt=prompt,
-                    max_tokens=200,
-                    timeout=120,
-                )
-            except Exception as e:
-                return f"OpenAI API error: {e}"
-            except requests.exceptions.ReadTimeout as e:
-                return f"Request timed out: {e}"
-            else:
-                if completions is None:
-                    return "Kunne ikke generere et sammendrag med OpenAI API."
-                else:
-                    summary = completions.choices[0].text.strip()
-                    if summary is None:
+                text_chunks = [text[i:i+4096] for i in range(0, len(text), 4096)]
+                summaries = []
+                for chunk in text_chunks:
+                    prompt = f"Please summarize the following article:\n{chunk}\n\nSummary:"
+                    completions = openai.Completion.create(
+                        engine="text-davinci-002",
+                        prompt=prompt,
+                        max_tokens=300,
+                    )
+                    if completions is None:
                         return "Kunne ikke generere et sammendrag med OpenAI API."
-                    
-                    # Print the summary after translation
-                    translator = googletrans.Translator()
-                    try:
-                        translation = translator.translate(summary, src="en", dest="no")
+                    else:
+                        summary = completions.choices[0].text.strip()
+                        summaries.append(summary)
+
+                translator = Translator()
+                try:
+                    for summary in summaries:
+                        translation = translator.translate(summary, src='en', dest='no')
                         if translation is not None and hasattr(translation, 'text'):
                             translated_text = translation.text
                             if os.path.exists(file_path):
@@ -81,9 +79,46 @@ def index():
                         else:
                             print("Translation failed: Unable to retrieve translated text") 
                             return "Kunne ikke oversette sammendraget."
-                    except AttributeError as e:
-                        print("Google Translate error:", e)
-                        return "Feil oppstod under oversettelsen av sammendraget."
+                except AttributeError as e:
+                    print("Google Translate error:", e)
+                    return "Feil oppstod under oversettelsen av sammendraget."
+                
+
+            #prompt = f"Please summarize the following article:\n{text}\n\nSummary:"
+            #try:
+                #completions = openai.Completion.create(
+                    #engine="text-davinci-002",
+                    #prompt=prompt,
+                    #max_tokens=200,
+                    #timeout=120,
+                #)
+            except Exception as e:
+                return f"OpenAI API error: {e}"
+            except requests.exceptions.ReadTimeout as e:
+                return f"Request timed out: {e}"
+            #else:
+                #if completions is None:
+                    #return "Kunne ikke generere et sammendrag med OpenAI API."
+                #else:
+                    #summary = completions.choices[0].text.strip()
+                    #if summary is None:
+                        #return "Kunne ikke generere et sammendrag med OpenAI API."
+                    
+                    # Print the summary after translation
+                    #translator = googletrans.Translator()
+                    #try:
+                        #translation = translator.translate(summary, src="en", dest="no")
+                        #if translation is not None and hasattr(translation, 'text'):
+                            #translated_text = translation.text
+                            #if os.path.exists(file_path):
+                                #os.remove(file_path)
+                            #return render_template('index.html', summary=translated_text)
+                        #else:
+                            #print("Translation failed: Unable to retrieve translated text") 
+                            #return "Kunne ikke oversette sammendraget."
+                    #except AttributeError as e:
+                        #print("Google Translate error:", e)
+                        #return "Feil oppstod under oversettelsen av sammendraget."
     return render_template('upload.html')
 
 if __name__ == '__main__':
