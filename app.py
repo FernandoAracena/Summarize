b800c2cf8e5b59f390667d038f60969bcb486a07
Fernando Aracena <aracenafernandou@gmail.comgit config --global user.name Fernando>
2023-05-22 13:11:00 +0200

    handling errors specifically

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 import docx2txt
 import openai
-import googletrans
 import requests
 import os
 from PyPDF2 import PdfReader
@@ -30,79 +29,99 @@ def handle_error(error, custom_message=None):
         return render_template('error.html', error=error_message)
 
 
+def save_file(file):
+    # Create the "uploads" folder if it doesn't exist
+    if not os.path.exists('uploads'):
+        os.makedirs('uploads')
+    
+    # Generate a unique filename
+    filename = f"{uuid.uuid4().hex}"
+    file_extension = os.path.splitext(file.filename)[1]  # Get the file extension
+    filename_with_extension = f"{filename}{file_extension}"
+    file_path = os.path.join('uploads', filename_with_extension)
+    
+    # Save the file to a temporary location
+    file.save(file_path)
+    return file_path
+
+
+def extract_text(file_path):
+    text = ''
+    if file_path.endswith('.docx'):
+        text = docx2txt.process(file_path)
+    elif file_path.endswith('.pdf'):
+        with open(file_path, 'rb') as f:
+            pdf_reader = PdfReader(f)
+            for page_num in range(len(pdf_reader.pages)):
+                page = pdf_reader.pages[page_num]
+                text += page.extract_text()
+    elif file_path.endswith('.txt'):
+        with open(file_path, 'r') as f:
+            text = f.read()
+    return text
+
+
+def generate_summary(text):
+    openai.api_key = API_KEY
+    text_chunks = [text[i:i+4096] for i in range(0, len(text), 4096)]
+    summaries = []
+    
+    for chunk in text_chunks:
+        prompt = f"Please summarize the following article:\n{chunk}\n\nSummary:"
+        completions = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=prompt,
+            max_tokens=300,
+        )
+        
+        if completions is None:
+            raise Exception("Unable to generate a summary with OpenAI API.")
+        
+        summary = completions.choices[0].text.strip()
+        summaries.append(summary)
+    
+    return summaries
+
+
+def translate_summary(summary):
+    translator = Translator()
+    translation = translator.translate(summary, src='en', dest='no')
+    
+    if translation is not None and hasattr(translation, 'text'):
+        return translation.text
+    
+    raise AttributeError("Unable to translate the summary.")
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Get the uploaded file from the user
         file = request.files['file']
-        # Create the "uploads" folder if it doesn't exist
-        if not os.path.exists('uploads'):
-            os.makedirs('uploads')
-        # Generate a unique filename
-        filename = f"{uuid.uuid4().hex}"
-        file_extension = os.path.splitext(file.filename)[1]  # Get the file extension
-        filename_with_extension = f"{filename}{file_extension}"
-        file_path = os.path.join('uploads', filename_with_extension)
-        # Save the file to a temporary location
-        file.save(file_path)
-
-        # Extract the text from the uploaded file
-        text = ''
-        if file_path.endswith('.docx'):
-            text = docx2txt.process(file_path)
-        elif file_path.endswith('.pdf'):
-            with open(file_path, 'rb') as f:
-                pdf_reader = PdfReader(f)
-                for page_num in range(len(pdf_reader.pages)):
-                    page = pdf_reader.pages[page_num]
-                    text += page.extract_text()
-        elif file_path.endswith('.txt'):
-            with open(file_path, 'r') as f:
-                text = f.read()
-        else:
-            return handle_error(Exception(), custom_message="Please select a .docx, .pdf, or .txt file.")
-
-        if not text:
-            return handle_error(Exception(), custom_message="No text found in the file.")
-        else:
-            # Authenticate with the OpenAI API and generate a summary of the text
-            openai.api_key = API_KEY
-
-            # Split the input text into smaller chunks and process each chunk separately
-            try:
-                text_chunks = [text[i:i+4096] for i in range(0, len(text), 4096)]
-                summaries = []
-                for chunk in text_chunks:
-                    prompt = f"Please summarize the following article:\n{chunk}\n\nSummary:"
-                    completions = openai.Completion.create(
-                        engine="text-davinci-002",
-                        prompt=prompt,
-                        max_tokens=300,
-                    )
-                    if completions is None:
-                        return handle_error(Exception(), custom_message="Unable to generate a summary with OpenAI API.")
-                    else:
-                        summary = completions.choices[0].text.strip()
-                        summaries.append(summary)
-
-                translator = Translator()
-                try:
-                    for summary in summaries:
-                        translation = translator.translate(summary, src='en', dest='no')
-                        if translation is not None and hasattr(translation, 'text'):
-                            translated_text = translation.text
-                            if os.path.exists(file_path):
-                                os.remove(file_path)
-                            return render_template('index.html', summary=translated_text)
-                        else:
-                            return handle_error(AttributeError(), custom_message="Unable to translate the summary.")
-                except AttributeError as e:
-                    return handle_error(e)
-            except Exception as e:
-                return handle_error(e)
-            except requests.exceptions.ReadTimeout as e:
-                return handle_error(e)
+        try:
+            file_path = save_file(file)
+            text = extract_text(file_path)
+            
+            if not text:
+                raise Exception("No text found in the file.")
+            
+            summaries = generate_summary(text)
+            translated_summaries = []
+            
+            for summary in summaries:
+                translated_summary = translate_summary(summary)
+                translated_summaries.append(translated_summary)
+            
+            os.remove(file_path)
+            return render_template('index.html', summary=translated_summaries)
+        
+        except AttributeError as e:
+            return handle_error(e)
+        
+        except Exception as e:
+            return handle_error(e, custom_message="Something went wrong during processing.")
+    
     return render_template('upload.html')
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
