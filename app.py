c27bef85005e1e442363ddb23c8b437eb0435860
Fernando Aracena <aracenafernandou@gmail.comgit config --global user.name Fernando>
2023-05-24 20:25:05 +0200

    re writing try except block to app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
+import requests
 from functions.file_processor import process_file
+from functions.error_handler import handle_error
 from functions.chunks_processor import process_chunks
 from functions.summaries_translator import translate_summaries
 import os
@@ -9,17 +11,25 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        file = request.files['file']
-        text, file_path = process_file(file)
+        try:
+            file = request.files['file']
+            text, file_path = process_file(file)
 
-        summaries = process_chunks(text)
+            summaries = process_chunks(text)
 
-        translated_summaries = translate_summaries(summaries)
+            translated_summaries = translate_summaries(summaries)
 
-        if os.path.exists(file_path):
-            os.remove(file_path)
+            if os.path.exists(file_path):
+                os.remove(file_path)
 
-        return render_template('index.html', summary=translated_summaries)
+            return render_template('index.html', summary=translated_summaries)
+        
+        except AttributeError as e:
+            return handle_error(e)
+        except requests.exceptions.ReadTimeout as e:
+            return handle_error(e)
+        except Exception as e:
+            return handle_error(e)
         
     return render_template('upload.html')
 
